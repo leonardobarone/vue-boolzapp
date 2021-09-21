acc3807354c41987a1d5b8b62fc4ec66d27d1240
leonardobarone <leonardobarone26@gmail.com>
2021-09-21 12:01:38 +0200

    impostato linear-gradient e container

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// BOOLZAPP - A (NOT VERY) INNOVATIVE MESSAGING PLATFORM
+
+const app = new Vue({
+    el: "#root",
+})
+
+// Milestone 1
+
+// 1. Replica della grafica con la possibilità di avere messaggi scritti dall'utente (verdi) e dall'interlocutore (bianco) assegnando due classi CSS diverse.
+
+// 2. Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto. 
